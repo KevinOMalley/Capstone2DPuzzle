964f6a9c1964e52d1fd2ba6d48939f02a57999f9
Aiden Lassiter <alassi1824@outlook.com>
2024-11-03 21:15:58 -0500

    Changed jetpack to only vertical, removed wall jump
    Cleaned up code, removed wall jump, wall jump cooldown now jump cooldown

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D boxCollider;
-    private float wallJumpCooldown;
+    private float JumpCooldown;
     private float horizontalInput;
     
 
@@ -30,6 +30,8 @@ public class PlayerMovement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         if (horizontalInput != 0) gameObject.transform.SetParent(null);
 
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
         // flip player when moving left-right
         if (horizontalInput > 0.01f)
             transform.localScale = new Vector3(1, 1, 1);
@@ -45,50 +47,11 @@ public class PlayerMovement : MonoBehaviour
             transformed = false;
         }
 
-
-        // wall jump logic
-        if (wallJumpCooldown > 0.2f)
+        if (Input.GetKey(KeyCode.Space) && isGrounded())
+            Jump();
+        else if (Input.GetKey(KeyCode.Space) && !isGrounded() && JumpCooldown > 0.2f && jetpackFuel > 0)
         {
-            body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
-            if (onWall() && !isGrounded())
-            {
-                body.gravityScale = 0;
-                body.velocity = Vector2.zero;
-            }
-            else
-                body.gravityScale = 7;
-
-            if (Input.GetKey(KeyCode.Space))
-                Jump();
-        }
-        else
-            wallJumpCooldown += Time.deltaTime;
-
-    }
-
-    private void Jump()
-    {
-        if (isGrounded())
-        {
-            body.velocity = new Vector2(body.velocity.x, jumpPower);
-            anim.SetTrigger("jump");
-        }
-        else if (onWall() && !isGrounded())
-        {
-            if (horizontalInput == 0)
-            {
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
-                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-            }
-            else
-                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
-
-            wallJumpCooldown = 0;
-        }
-        else if (!isGrounded() && jetpackFuel > 0) // jetpack logic 
-        {
-            body.velocity = new Vector2(body.velocity.x, jumpPower / 2);
+            body.velocity = new Vector2(0, jumpPower / 2);
             if(transformed == false)
             {
                 anim.SetTrigger("transform");
@@ -99,6 +62,19 @@ public class PlayerMovement : MonoBehaviour
                 anim.SetTrigger("rocket");
             }
         }
+        else
+            JumpCooldown += Time.deltaTime;
+        
+    }
+
+    private void Jump()
+    {
+        if (isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        JumpCooldown = 0;
     }
 
     private bool isGrounded()
