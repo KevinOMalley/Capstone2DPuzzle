18894d4309930725376693faba7cd59946ff21df
Aiden Lassiter <alassi1824@outlook.com>
2024-11-14 09:56:55 -0500

    Revert "Made it so you now lose hearts when you hit a spike, and respawn when…"

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -2,19 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] private float startingHealth;
-    public int respawn;
     public float currentHealth { get; private set; }
-    private Animator anim;
 
     private void Awake()
     {
         currentHealth = startingHealth;
-        anim = GetComponent<Animator>();
+
     }
 
     public void TakeDamage(float _damage)
@@ -24,13 +21,12 @@ public class Health : MonoBehaviour
         if(currentHealth > 0 )
         {
             // player hurt
-            anim.SetTrigger("hurt");
         }
         else
         {
             // player dead
-            anim.SetTrigger("die");
-            SceneManager.LoadScene(respawn);
         }
     }
+
+
 }
